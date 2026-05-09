a719dcc84ec296d59c790b69eaa4b9d1c2b2a05a
arabangoo <indosa2@gmail.com>
2026-05-10 08:14:49 +0900

    agent_update_0510

---
+++ goose/crates/goose-cli/src/commands/mod.rs
@@ -0,0 +1,11 @@
+pub mod configure;
+pub mod doctor;
+pub mod gateway;
+pub mod info;
+pub mod plugin;
+pub mod project;
+pub mod recipe;
+pub mod schedule;
+pub mod session;
+pub mod term;
+pub mod update;
